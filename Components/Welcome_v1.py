15f2e04c69eec218a3c6d549891be2f2df6d8497
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-03-23 15:04:28 +1300

    Created Welcome Component
    Created Version 1 of the welcome message and in Version 2 made it into a function and uploaded to main code

---
+++ Components/Welcome_v1.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#List of random names
+names = ["Caleb", "Shawn", "Sean", "Gian", "Jayden", "Ryan", "Daniel", "Danett", "Hannah", "Teresa" ]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to my Laptop Shop ***")
+print("*** my name is",name,"***")
+print("*** I will be here to help you order the right and perfect laptop for you ***")
