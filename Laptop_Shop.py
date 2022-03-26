3406b9117604f815c593268d7247fc9386983347
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-03-26 20:31:23 +1300

    Updated Laptop Shop Bot
    Integrated Menu for Pickup function a into Main Code as and added function for validating if a question is blank. Also added bugs that were going to be fixed later on

--- Laptop_Shop.py
+++ Laptop_Shop.py
@@ -1,9 +1,25 @@
 #Laptop Shop Bot
+# 26.03.22
+# Bugs - Phone Number allows letters
+#      - Name allows numbers
+
 import random
 from random import randint
 
 #List of random names
 names = ["Caleb", "Shawn", "Sean", "Gian", "Jayden", "Ryan", "Daniel", "Danett", "Hannah", "Teresa" ]
+# Customer details dictionary
+customer_details = {}
+
+# validates inputs to check if they are blank
+def not_blank(question):
+    valid = False
+    while not valid:
+        response = input(question)
+        if response != "":
+            return response.title()
+        else:
+            print("This cannot be blank")
 
 # Welcome message with random name
 def welcome():
@@ -15,12 +31,12 @@ def welcome():
     '''
     num = randint(0,9)
     name = (names[num])
-    print("*** Welcome to Dream Pizza ***")
+    print("*** Welcome to my Laptop Shopa ***")
     print("*** my name is ",name, "***")
-    print("*** I will be here to help you order your delicious Dream Pizza ***")
+    print("*** I will be here to help you order the right and perfect laptop for you ***")
 
 # Menu for Pickup and Delivery
-def pickup():
+def order_type():
     print ("Do you want your order delivered or are you picking it up")
     print ("For pickup please enter 1") 
     print ("For delivery please enter 2")
@@ -30,8 +46,8 @@ def pickup():
             if delivery >= 1 and delivery <= 2:
                 if delivery == 1:
                     print ("Pickup")
+                    pickup()
                     break
-
                 elif delivery == 2:
                     print ("Delivery")
                     break
@@ -44,7 +60,15 @@ def pickup():
 
 
 # Pickup information - Name and Phone
+def pickup():
+    question = ("Please enter your name ")
+    customer_details['name'] = not_blank(question )
+    #print(customer_details['name'])
 
+    question = ("Please enter your phone number ")
+    customer_details['phone'] = not_blank(question)
+    #print(customer_details['phone'])
+    print(customer_details)
 
 
 
@@ -92,6 +116,7 @@ def main():
     Returns: None
     '''
     welcome()
+    order_type()
     pickup()
 
 
