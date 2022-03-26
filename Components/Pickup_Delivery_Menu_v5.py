783cabb072baa7b452000aec40a2f400e2124108
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-03-26 19:37:21 +1300

    Update Pickup_Delivery_Menu_v5.py
    fixing errors

--- Components/Pickup_Delivery_Menu_v5.py
+++ Components/Pickup_Delivery_Menu_v5.py
@@ -3,10 +3,8 @@
 # Bug - need it so that it only accepts 1 or 2
 
 print ("Do you want your order delivered or are you picking it up")
-
 print ("For pickup please enter 1") 
 print ("For delivery please enter 2")
-
 while True:
     try:
         delivery = int(input("Please enter a number "))
