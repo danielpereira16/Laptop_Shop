259d3c539a1d057e19ace46188d614dbb66100fa
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-04-03 15:23:09 +1200

    Updated Laptop Shop Bot
    Integrated Laptop Ordering as a function into Main Code. Removed extra spaces

--- Laptop_Shop.py
+++ Laptop_Shop.py
@@ -22,6 +22,11 @@ laptop_names = ['Asus 14" Laptop - Intel Celeron 4GB-RAM 128GB','Asus 15.6" Lapt
 #List of laptop prices
 laptop_prices = [688,1884,1994,2149,2549,1584,1994,3997,3128,1698,836,1298,2499,2549,2699,994,2388,1984]
 
+#list to store ordered laptops
+order_list = []
+#list to store laptop prices
+order_cost = []
+
 # Customer details dictionary
 customer_details = {}
 
@@ -113,12 +118,40 @@ def list():
     for count in range (number_laptops):
         print("{} {} ${:.2f}"   .format(count+1,laptop_names[count],laptop_prices[count]))
 
-# Choose total number of items
-
+# Choose total number of items - max 20
 # Item(s) ordered - from item list - print each ordered item with cost
-
-
-
+def order_laptops():
+    # ask for total number of laptops for order
+    print("Please note that due to covid, there has been delays in shipping and therefore you can only order a maximum of 20 Laptops per order")
+    num_laptops = 0
+    while True:
+        try:
+            num_laptops = int(input("How many Laptops do you want to order? "))
+            if num_laptops >= 1 and num_laptops <= 20:
+                break
+            else:
+                print("Your order must between 1 and 20")
+        except ValueError:
+            print ("That is not a valid number")
+            print ("Please enter number enter between 1 and 20 ")
+    # Choose laptops from the list
+    for item in range(num_laptops):
+        while num_laptops > 0:
+            while True:
+                try:
+                    laptops_ordered = int(input("Please choose your laptops by entering the number from the list "))
+                    if laptops_ordered >= 1 and laptops_ordered <= 18:
+                        break
+                    else: 
+                        print("Your number must be between 1 and 20")
+                except ValueError:
+                        print ("That is not a valid number")
+                        print ("Please enter number enter between 1 and 20 ") 
+            laptops_ordered = laptops_ordered -1
+            order_list.append(laptop_names[laptops_ordered])
+            order_cost.append(laptop_prices[laptops_ordered])    
+            print("{} ${:.2f}" .format(laptop_names[laptops_ordered],laptop_prices[laptops_ordered]))
+            num_laptops = num_laptops-1
 
 # Print order out - including if the order is pickup or delivery and names and prices of each item - total cost including any delivery charge
 
@@ -146,6 +179,6 @@ def main():
     welcome()
     order_type()
     list()
-    
+    order_laptops()
 
 main()
