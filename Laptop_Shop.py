997e50ed01efbcaa449ca2dba2d0407a2c0a5c22
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-04-09 18:32:37 +1200

    Updated Laptop Shop Bot
    Updated Laptop Shop Bot Main Code where I have integrated the confirm and cancel order function

--- Laptop_Shop.py
+++ Laptop_Shop.py
@@ -185,9 +185,30 @@ def print_order(del_pick):
     print(f"${total_cost:.2f}")
 
 # Ability to cancel or proceed with order
+def confirm_order():
+    print ("Please Confirm Your Order")
+    print ("To Confirm please enter 1") 
+    print ("To Cancel please enter 2")
+    print()
+    while True:
+        try:
+            confirm = int(input("Please enter a number "))
+            if confirm >= 1 and confirm <= 2:
+                if confirm == 1:
+                    print ("Order Confirmed")
+                    print ("Your order will be sent to our Store")
+                    print ("It will be ready soon")
+                    break
 
-
-
+                elif confirm == 2:
+                    print ("Order Cancelled")
+                    print ("You can restart your order or exit the BOT")
+                    break
+            else:
+                print("Number must be 1 or 2")
+        except ValueError:
+            print ("That was not a valid input")
+            print ("Please enter 1 or 2 ")
 
 # Option for new order or to exit
 
@@ -207,5 +228,6 @@ def main():
     list()
     order_laptops()
     print_order(del_pick)
+    confirm_order()
 
 main()
