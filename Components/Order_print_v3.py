20d7add95b8d05f8e6a36bc9fdc649f8f84ae7b7
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-04-09 14:59:34 +1200

    Updated Version 3 of Order print
    Rewriting code

--- Components/Order_print_v3.py
+++ Components/Order_print_v3.py
@@ -15,7 +15,4 @@ count = 0
 for item in order_list:
     print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
     count = count+1
-count = 0
-for item in order_list:
-    print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
-    count = count+1
+
