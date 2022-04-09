d2f8525536bdbbcb651bb2e47c09c0041397e7d6
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-04-09 15:17:28 +1200

    Created version 4 of order print
    Added formatting to print order and added total cost. Developed it into a function

---
+++ Components/Order_print_v4.py
@@ -0,0 +1,24 @@
+#list to store ordered laptops
+order_list = ['Asus 14" Laptop - Intel Celeron 4GB-RAM 128GB','Asus 15.6" Laptop - Intel Core i7 16GB-RAM 512GB-SSD',
+'Asus TUF 15.6" Gaming Laptop - Intel Core i5 8GB-RAM 512GB-SSD','Apple MacBook Air 13" with M1 Chip 512GB',
+'Apple MacBook Pro 13" with M1 Chip 512GB','HP 15.6" Laptop - Intel Core i5 8GB-RAM 256GB-SSD']
+#list to store pizza prices
+order_cost = [688, 1884, 1994, 2149, 2549, 1584]
+
+cust_details = {'Name':'Mark', 'Phone':'0214635678', 'House':'34', 'Street':'Dragon', 'Surburb':'Flat Bush'}
+
+def print_order():
+    total_cost = sum(order_cost)
+    print("Customer Details")
+    print(f"Customer Name: {cust_details['Name']} \nCustomer Phone: {cust_details['Phone']} \nCustomer Address: {cust_details['House']} {cust_details['Street']} {cust_details['Surburb']}")
+    print()
+    print("Order Details")
+    count = 0
+    for item in order_list:
+        print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
+        count = count+1
+    print()
+    print("Order Cost Details")
+    print(f"${total_cost:.2f}")
+
+print_order()
