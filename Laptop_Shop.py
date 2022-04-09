71956d862b4f410dcc7d3b95013ad9852f0a75b1
danielpereira16 <17022@my.sanctamaria.school.nz>
2022-04-09 19:38:53 +1200

    Updated Laptop Shop Bot
    Updated Laptop Shop Bot Main Code with integrating the the new order or exit program code into the main code. Linked New_Exit function into confirm or cancel order function

--- Laptop_Shop.py
+++ Laptop_Shop.py
@@ -3,6 +3,7 @@
 # Bugs - Phone Number allows letters
 #      - Name allows numbers
 
+import sys
 import random
 from random import randint
 
@@ -183,13 +184,13 @@ def print_order(del_pick):
             total_cost = total_cost + 9
     print("Total Order Cost")
     print(f"${total_cost:.2f}")
+    print()
 
 # Ability to cancel or proceed with order
 def confirm_order():
     print ("Please Confirm Your Order")
     print ("To Confirm please enter 1") 
     print ("To Cancel please enter 2")
-    print()
     while True:
         try:
             confirm = int(input("Please enter a number "))
@@ -198,11 +199,15 @@ def confirm_order():
                     print ("Order Confirmed")
                     print ("Your order will be sent to our Store")
                     print ("It will be ready soon")
+                    print()
+                    new_exit()
                     break
 
                 elif confirm == 2:
                     print ("Order Cancelled")
                     print ("You can restart your order or exit the BOT")
+                    print()
+                    new_exit()
                     break
             else:
                 print("Number must be 1 or 2")
@@ -211,9 +216,35 @@ def confirm_order():
             print ("Please enter 1 or 2 ")
 
 # Option for new order or to exit
+def new_exit():
+    print ("Do you want to start another Order or Exit?")
+    print ("To start another order enter 1") 
+    print ("To exit the bot enter 2")
+    while True:
+        try:
+            confirm = int(input("Please enter a number "))
+            if confirm >= 1 and confirm <= 2:
+                if confirm == 1:
+                    print ("New Order")
+                    print()
+                    order_list.clear()
+                    order_cost.clear()
+                    customer_details.clear()
+                    main()
+                    break
 
-
-
+                elif confirm == 2:
+                    print ("Exit")
+                    order_list.clear()
+                    order_cost.clear()
+                    customer_details.clear()
+                    sys.exit
+                    break
+            else:
+                print("Number must be 1 or 2")
+        except ValueError:
+            print ("That was not a valid input")
+            print ("Please enter 1 or 2 ")
 
 # Main Function
 def main():
